356f53c45d9aa87cfa5f27a5db95f7db109b9de2
Umar Farooq Bajwa <email": null>
2016-06-12 18:09:09 -0700

    Fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-    root 'welcome#index'
+    # root 'welcome#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+root 'articles#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 resources :articles do
   resources :comments
 end
+
   #   resources :products
 
   # Example resource route with options
